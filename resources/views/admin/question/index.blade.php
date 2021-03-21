876f9bfa6bde17ffa175c73b0b642254b385e6b1
upbook26 <upbook.26@gmail.com>
2021-03-21 12:33:22 +0900

    スタイル変更

--- resources/views/admin/question/index.blade.php
+++ resources/views/admin/question/index.blade.php
@@ -1,21 +1,40 @@
 <x-admin-layout>
-    <h2>質問一覧</h2>
-    <div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>タイトル</th>
-                    <th>質問者</th>
-                    <th>投稿日時</th>
-                </tr>
-            </thead>
-            @foreach ($questions as $question)
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">プログラミング質問掲示板</a>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home　></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">質問一覧</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <br>
+    <div class="container-fluid">
+        <h4>質問一覧</h4>
+
+        <div>
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>タイトル</th>
+                        <th>質問者</th>
+                        <th>投稿日時</th>
+                    </tr>
+                </thead>
+                @foreach ($questions as $question)
                 <tr>
                     <td>{{$question->title}}</td>
                     <td>{{$question->user->name}}</td>
                     <td>{{$question->created_at}}</td>
                 </tr>
-            @endforeach
-        </table>
+                @endforeach
+            </table>
+        </div>
     </div>
 </x-admin-layout>

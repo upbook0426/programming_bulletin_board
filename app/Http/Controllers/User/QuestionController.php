0e6a87d770e221dbdaa77d3b969eae3d7c3bd919
Megumi Shimizu <stakanari987@gmail.com>
2021-04-02 20:31:16 +0900

    フォーマットの修正

--- app/Http/Controllers/User/QuestionController.php
+++ app/Http/Controllers/User/QuestionController.php
@@ -2,35 +2,36 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
-use App\Http\Requests\QuestionRequest;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Question;
 use App\Enums\PublishStateType;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\QuestionRequest;
+use App\Models\Question;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
-   	public function index() {
-         $status = PublishStateType::toSelectArray();
-   		return view('question.index', compact('status'));
-   	}
+    public function index()
+    {
+        $status = PublishStateType::toSelectArray();
+        return view('question.index', compact('status'));
+    }
 
-   	public function store(QuestionRequest $request) {
+    public function store(QuestionRequest $request)
+    {
+        $question = new Question();
+        $question->user_id = Auth::user()->id;
+        $question->title = $request->input('title');
+        $question->body = $request->input('body');
+        $question->status = $request->input('status');
 
-   		$question = new Question();
-   		$question->user_id = Auth::user()->id;
-   		$question->title = $request->input('title');
-   		$question->body = $request->input('body');
-   		$question->status = $request->input('status');
-
-         if ($request->input('status') == 'public'){
+        if ($request->input('status') == 'public'){
             $question->released_at = now();
-         }
+        }
 
-   		$question->save();
+        $question->save();
 
-   		return redirect()->back()
-   			->with('status', '質問を投稿しました。');
-   	}
+        return redirect()->back()
+            ->with('status', '質問を投稿しました。');
+    }
 }

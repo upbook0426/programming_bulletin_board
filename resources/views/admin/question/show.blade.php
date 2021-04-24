4889c4d41018d4911590ab1fe50dc78299988295
Megumi Shimizu <stakanari987@gmail.com>
2021-04-24 17:02:09 +0900

    不要な改行削除

--- resources/views/admin/question/show.blade.php
+++ resources/views/admin/question/show.blade.php
@@ -2,24 +2,16 @@
     <x-header></x-header>
     <br>
     <div class="container-fluid">
-
         <div class="card">
             <div class="card-body">
                 <h4>{{$question->title}}</h4>
-                <p><a href="#">{{htmlspecialchars($question->user->name, ENT_QUOTES, "UTF-8")}}</a></p>
+                <p><a href="#">{{$question->user->name}}</a></p>
                 <p>{{$question->created_at}}</p>
-            </div>
-
-            <hr>
-
-            <x-label>
+                <hr>
                 <h5>質問内容</h5>
                 <pre>{{$question->body}}</pre>
-            </x-label>
+            </div>
         </div>
-
-        
-
     </div>
 </x-layouts.admin>
 

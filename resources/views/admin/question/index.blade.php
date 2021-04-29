3d534467cafaf2f90684bedf086c022208894ebc
Megumi Shimizu <stakanari987@gmail.com>
2021-04-29 10:26:17 +0900

    管理者側を元に戻す

--- resources/views/admin/question/index.blade.php
+++ resources/views/admin/question/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app>
+<x-layouts.admin>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">プログラミング質問掲示板</a>
@@ -12,9 +12,7 @@
     <br>
     <div class="container-fluid">
         <h4>質問一覧</h4>
-
         <div>
-
             <table class="table table-bordered">
                 <thead>
                     <tr>
@@ -24,47 +22,20 @@
                     </tr>
                 </thead>
                 @foreach ($questions as $question)
-                @if ($question->status == 'public')
-                <tr>
-                    <td>
-                        <a href="{{route('admin.questions.show', ['question'=> $question])}}">{{$question->title}}</a>
-                        {{--
-                        @if ($question->status == 'public')
-                            <a href="{{route('admin.questions.show', ['question'=> $question])}}">{{$question->title}}</a>
-                        @else
-                            <span>{{$question->title}}</span>
-                            <span class="badge badge-secondary float-right">非公開</span>
-                        @endif-->--}}
-                    </td>
-                    <td>{{$question->user->name}}</td>
-
-                    <!--差分時間表示_年-->
-                    @if(floor($question->timeDifference/(60*60*24*365)) > 0)
-                    <td>{{floor($question->timeDifference/(60*60*24*365))}}年前</td>
-                    <!--月-->
-                    @elseif(floor($question->timeDifference/(60*60*24*30)) > 0)
-                    <td>{{floor($question->timeDifference/(60*60*24*30))}}ヶ月前</td>
-                    <!--週間-->
-                    @elseif(floor($question->timeDifference/(60*60*24*7)) > 0)
-                    <td>{{floor($question->timeDifference/(60*60*24*7))}}週間前</td>
-                    <!--日-->
-                    @elseif(floor($question->timeDifference/(60*60*24)) > 0)
-                    <td>{{floor($question->timeDifference/(60*60*24))}}日前</td>
-                    <!--時間-->
-                    @elseif(floor($question->timeDifference/(60*60)) > 0)
-                    <td>{{floor($question->timeDifference/(60*60))}}時間前</td>
-                    <!--分-->
-                    @elseif(floor($question->timeDifference/(60)) > 0)
-                    <td>{{floor($question->timeDifference/(60))}}分前</td>
-                    <!--分またはその他-->
-                    @else
-                    <td>0分前</td>
-                    @endif
-                    
-                </tr>
-                @endif
+                    <tr>
+                        <td>
+                            @if ($question->status == 'public')
+                                <a href="{{route('admin.questions.show', ['question'=> $question])}}">{{$question->title}}</a>
+                            @else
+                                <span>{{$question->title}}</span>
+                                <span class="badge badge-secondary float-right">非公開</span>
+                            @endif
+                        </td>
+                        <td>{{$question->user->name}}</td>
+                        <td>{{$question->created_at}}</td>
+                    </tr>
                 @endforeach
             </table>
         </div>
     </div>
-</x-layouts.app>
+</x-layouts.admin>
